aece39f6570f3540631ceb31186471dccd64b318
Nazanin Sabri <nazanin.sabrii@gmail.com>
2018-07-31 15:09:18 +0430

    change urls and forms

--- blogFormat/posts/urls.py
+++ blogFormat/posts/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.posts_home, name="blog"),
-    path('/detail/', views.post_detail, name="detail"),
-    path('/create/', views.post_create, name="create"),
-    path('/update/', views.post_update, name="update"),
-    path('/delete/', views.post_delete, name="delete"),
+    path('<int:id>/', views.post_detail, name="blog_detail"),
+    path('create/', views.post_create, name="blog_create"),
+    path('update/', views.post_update, name="blog_update"),
+    path('delete/', views.post_delete, name="blog_delete"),
 ]
--- blogFormat/posts/views.py
+++ blogFormat/posts/views.py
@@ -1,15 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Post
+from .forms import PostForm
 # Create your views here.
 
 def posts_home(request):
-    return HttpResponse("<h1>Hello</h1>")
+    allPosts = Post.objects.all()
+    context = {"posts": allPosts}
+    return render(request, "blog.html", context)
 
-def post_detail(request):
-    return HttpResponse("<h1>Hello</h1>")
+def post_detail(request, id=None):
+    thisPost = get_object_or_404(Post, id=id)
+    context = {"post": thisPost}
+    return render(request, "post_detail.html", context)
     
 def post_create(request):
-    return HttpResponse("<h1>Hello</h1>")
+    # if request.user.is_authenticated():
+    form = PostForm()
+    context = {
+        "form": form
+    }
+    return render(request, "create_post.html", context)
+    # else:
+    #     return HttpResponse("<h1>Sorry you do not have access to this page!</h1>")
 
 def post_update(request):
     return HttpResponse("<h1>Hello</h1>")
